2253516029e68472d3d1d00e145e87fcfe2a2801
arcur <arcuredev@gmail.com>
2025-05-08 21:35:15 +0200

    Un

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'un',
+    loadComponent: () => import('./un/un.component').then(c => c.UnComponent),
+  }
+];
---
+++ src/app/un/un.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {LeafletModule} from '@bluehalo/ngx-leaflet';
+
+@Component({
+  selector: 'app-un',
+  imports: [LeafletModule],
+  templateUrl: './un.component.html',
+  styleUrl: './un.component.css'
+})
+export class UnComponent {
+
+}
